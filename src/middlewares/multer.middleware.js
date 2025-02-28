0f753b8d325deba50968edebbb7208c00f6bf99c
Aishwarya Ghorpade <aishwaryaghorpade4@gmail.com>
2025-02-28 22:36:57 +0530

    cloudinary and multer

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,19 @@
+import multer from "multer";
+
+/*Multer is a middleware for handling multipart/form-data, which is used for uploading files.
+It provides functionality to store files either in memory (RAM) or on disk (filesystem).  */
+
+// 2 provide 2 types of space 1)diskStorage ,2)memoryStorage.any one can use
+
+const storage=multer.diskStorage({  // storage engine for multer
+    destination:function(req,file,cb){
+        cb(null,"./public/temp")
+    },
+    filename:function(req,file,cb){
+        cb(null,file.originalname)
+    }
+})   //This storage engine saves the file on the local disk. It allows customization of where the files should be saved and how the filename should be determined.
+
+export const upload=multer({     //Configuring Multer with the storage Engine:
+    storage
+})
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,26 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs"
+
+    // Configuration
+    cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret: process.env.CLOUDINARY_API_SECRET 
+    });
+
+    const uploadOnCloudinary=async(localFilePath)=>{
+        if(!localFilePath)  return null
+        try {
+            const response=await cloudinary.uploader.upload(localFilePath,{
+                resourse_type:"auto"
+            })
+            //file is uploaded on cloadinary
+            console.log("file is uploaded on cloadinary",response.url)
+            return response
+        } catch (error) {
+            fs.unlinkSync(localFilePath)  //remove the locally saved temporary file as the upload operation got failed
+            return null
+        }
+    }
+
+export {uploadOnCloudinary}
